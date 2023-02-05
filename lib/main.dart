8f8636021b9c9871abc2359aea32635aa8e6b557
mustafa <mryuksel5@gmail.com>
2023-02-05 22:20:41 +0300

    karanlik, aydinlik map assests

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     debugPrint("deneme");
     return const MaterialApp(
+
       debugShowCheckedModeBanner: false,
       home: Harita(),
     );
